77568b8f94435682ced3eaabfce765fcf5dbb031
Getriax <niki1234bez5@gmail.com>
2017-10-05 20:22:51 +0200

    security

--- src/main/java/com/demo/rest/DemoApplication.java
+++ src/main/java/com/demo/rest/DemoApplication.java
@@ -1,13 +1,8 @@
 package com.demo.rest;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+
 @SpringBootApplication
 public class DemoApplication {
 
